67efe0741b8236e56a5a786703526bbdc8d6998d
Raytwo <Raytwo@users.noreply.github.com>
2023-09-13 20:40:16 +0200

    Update engine.rs

--- src/engine.rs
+++ src/engine.rs
@@ -1,10 +1,10 @@
 #[repr(C)]
 #[derive(Clone, Copy, Debug)]
 pub struct Color {
-    r: f32,
-    g: f32,
-    b: f32,
-    a: f32,
+    pub r: f32,
+    pub g: f32,
+    pub b: f32,
+    pub a: f32,
 }
 
 #[repr(C)]

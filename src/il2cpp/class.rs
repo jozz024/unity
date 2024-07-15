8d37c14e28fea4d0580663285ab6652d85767b8c
Raytwo <Raytwo@users.noreply.github.com>
2024-07-15 18:23:57 +0200

    Update class.rs

--- src/il2cpp/class.rs
+++ src/il2cpp/class.rs
@@ -109,9 +109,9 @@ impl Il2CppClass {
 
         unsafe {
             // Malloc kind is "Normal" here, meaning the class and its inner pointers can be managed and freed by the Garbage Collector (BoehmGC)
-            let dest = gc_malloc_kind(size, 1) as *mut Il2CppClass;
+            let dest = gc_malloc_kind(size, 1);
             memcpy(dest, self, size);
-            &mut *dest
+            dest
         }
     }
 
